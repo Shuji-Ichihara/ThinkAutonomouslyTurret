9bdb1a0c7c12259805b8244dcd4e053277cef040
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-07-01 10:24:52 +0900

    Create SingletonMonoBehaviour
    シングルトンオブジェクトの基底クラスを作成した。

---
+++ ThinkAutonomouslyTurret/Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Type t = typeof(T);
+                _instance = (T)FindObjectOfType(t);
+                if (_instance == null)
+                {
+                    Debug.LogError(t + "をアタッチしている GameObject はありません。");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        // 他の GameObject にアタッチされているか調べる
+        // アタッチされている場合は破棄する
+        if (this != _instance)
+        {
+            Destroy(this);
+            Debug.LogError(
+                typeof(T)
+                + "は既に他の GameObject にアタッチされている為、コンポーネントを破棄しました。"
+                + "アタッチされている GameObject は" + _instance.gameObject.name + "です。");
+            return;
+        }
+    }
+}

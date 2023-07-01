65054c32381f12016882444601a583bfaa0c4c79
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-07-01 10:57:38 +0900

    Create PoolingObject
    プールするオブジェクトに必要なコンポーネントを作成した。

---
+++ ThinkAutonomouslyTurret/Assets/Scripts/PoolingObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PoolingObject : MonoBehaviour
+{
+    private Transform _transform = null;
+    
+    /// <summary>
+    /// このコンポーネントを持つオブジェクトのプール作成に使用
+    /// </summary>
+    /// <param name="spawnPosition">このオブジェクトの座標</param>
+    public void InitBullet(Vector3 spawnPosition)
+    {
+        _transform = GetComponent<Transform>();
+        _transform.position = spawnPosition;
+        gameObject.SetActive(false);
+    }
+}

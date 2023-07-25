0ec57eff1737a8b0364f08693b9d05b09e99da8a
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-07-25 17:57:50 +0900

    Create Bullet.cs
    砲弾のスクリプトを作成した。

---
+++ ThinkAutonomouslyTurret/Assets/Scripts/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    #region Move
+    // Bullet の速度
+    [SerializeField]
+    private float _bulletSpeed = 60.0f;
+    // Bullet の移動距離
+    private float _moveBulletDistance = 0.0f;
+    #endregion
+
+    [SerializeField]
+    private int _damage = 10;
+    public int Damage => _damage;
+
+    private Rigidbody _rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        MoveBullet();
+    }
+
+    /// <summary>
+    /// Bullet プレハブの移動
+    /// </summary>
+    private void MoveBullet()
+    {
+        _moveBulletDistance += _bulletSpeed * Time.deltaTime;
+        transform.position += CanonController.Instance.BulletSpawnPoint.up * _moveBulletDistance;
+        if(_moveBulletDistance > 200.0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        _moveBulletDistance = 0.0f;
+    }
+}

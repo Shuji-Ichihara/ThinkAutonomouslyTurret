6a478b8e1e3d921e79884ab255c90665a90b03e4
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-09-03 11:17:24 +0900

    Create TargetController
    的のスクリプトを作成した。

---
+++ ThinkAutonomouslyTurret/Assets/Scripts/TargetController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    // 的のスコア
+    [SerializeField]
+    private int _keepScore = 10;
+    // 的の耐久値
+    public int EndurancePoint = 0;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var bullet = other.gameObject.GetComponent<Bullet>();
+        HitBullet(bullet);
+    }
+
+    /// <summary>
+    /// 弾丸が当たったらスコア上昇
+    /// </summary>
+    /// <param name="bullet">Bullet スクリプト</param>
+    private void HitBullet(Bullet bullet)
+    {
+        EndurancePoint -= bullet.Damage;
+        if(EndurancePoint < 0)
+        {
+            GameManager.Instance.CountUpScore(_keepScore);
+            gameObject.SetActive(false);
+        }
+    }
+}

5b359f365d4f57c0b00e9ea4f0a001ff0c7e556b
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-10-24 08:37:54 +0900

    Update Bullet.cs
    弾を非アクティブ化する処理を追記した。また、弾の移動をRigidbody を使用した物理演算に変更した。

--- ThinkAutonomouslyTurret/Assets/Scripts/Bullet.cs
+++ ThinkAutonomouslyTurret/Assets/Scripts/Bullet.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
     #region Move
-    // Bullet の速度
+    // Bullet に加える力
     [SerializeField]
-    private float _bulletSpeed = 60.0f;
+    private float _bulletMoveForce = 60.0f;
     // Bullet の移動距離
     private float _moveBulletDistance = 0.0f;
     #endregion
-
+    // 弾のダメージ
     [SerializeField]
     private int _damage = 10;
     public int Damage => _damage;
@@ -24,6 +22,7 @@ public class Bullet : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
         _rb.useGravity = false;
+        _rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
     }
 
     private void FixedUpdate()
@@ -36,16 +35,28 @@ public class Bullet : MonoBehaviour
     /// </summary>
     private void MoveBullet()
     {
-        _moveBulletDistance += _bulletSpeed * Time.deltaTime;
-        transform.position += CannonController.Instance.BulletSpawnPoint.up * _moveBulletDistance;
-        if(_moveBulletDistance > 200.0f)
+        _rb.AddForce(CannonController.Instance.BulletSpawnPoint.up * _bulletMoveForce, ForceMode.Impulse);
+        _moveBulletDistance = Vector3.Distance(transform.position, GameManager.Instance.Cannon.transform.position);
+        // Cannon との距離が離れたら非アクティブ化する
+        if (_moveBulletDistance > 200.0f)
         {
             gameObject.SetActive(false);
         }
     }
 
-    private void OnEnable()
+    /// <summary>
+    /// 与えた力を 0 にする
+    /// </summary>
+    private void OnDisable()
     {
-        _moveBulletDistance = 0.0f;
+        _rb.velocity = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Target"))
+        {
+            gameObject.SetActive(false);
+        }
     }
 }

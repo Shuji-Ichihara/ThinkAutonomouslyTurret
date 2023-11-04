f137c86056b1094f47b70ea0392d29d5b9f861d8
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-11-04 10:37:56 +0900

    Create TitleSceneManager
    タイトルシーンのマネージャークラスを作成した。

---
+++ ThinkAutonomouslyTurret/Assets/Scripts/TitleScene/TitleSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TitleSceneManager : SingletonMonoBehaviour<TitleSceneManager>
+{
+    // シーン変更可能フラグ
+    private bool _isChangedScene = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && _isChangedScene == true)
+        {
+            SceneStateManager.Instance.ChangeScene(SceneState.GameScene);
+            _isChangedScene = false;
+        }
+    }
+
+    /// <summary>
+    /// シーン変更が可能なフラグを有効にする
+    /// </summary>
+    public void IsActivatedChangeScene()
+    {
+        _isChangedScene = true;
+    }
+}

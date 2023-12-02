9f643c11bb442172219318d4f667030553b6b472
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-12-02 10:46:11 +0900

    Create ResultSceneManager
    リザルトシーンの管理クラスを作成した。

---
+++ ThinkAutonomouslyTurret/Assets/Scripts/ResultScene/ResutlSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ResutlSceneManager : SingletonMonoBehaviour<ResutlSceneManager>
+{
+    // シーン遷移が可能になるフラグ
+    private bool _isActivatedChangeScene = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isActivatedChangeScene == true)
+        {
+            SceneStateManager.Instance.ChangeScene(SceneState.TitleScene);
+            _isActivatedChangeScene = false;
+        }
+    }
+
+    public void IsActivatedChageScene()
+    {
+        _isActivatedChangeScene = true;
+    }
+}

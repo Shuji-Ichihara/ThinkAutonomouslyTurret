a085a1e481faaae902e40930d1c19ce90b231d04
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-12-02 11:33:16 +0900

    Update TitleSceneManager
    タイトルシーンが開始した瞬間から BGM を流すようにした。

--- ThinkAutonomouslyTurret/Assets/Scripts/TitleScene/TitleSceneManager.cs
+++ ThinkAutonomouslyTurret/Assets/Scripts/TitleScene/TitleSceneManager.cs
@@ -5,11 +5,17 @@ public class TitleSceneManager : SingletonMonoBehaviour<TitleSceneManager>
     // シーン遷移が可能になるフラグ
     private bool _isChangedScene = false;
 
+    private void Start()
+    {
+        AudioManager.Instance.PlayBGM(BGMType.TitleBGM);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space) && _isChangedScene == true)
         {
+            AudioManager.Instance.PlaySE(SEType.PressKey);
             SceneStateManager.Instance.ChangeScene(SceneState.GameScene);
             _isChangedScene = false;
         }

451c92227025f23b904e90c6809eb5b8943107f0
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-12-04 00:56:00 +0900

    Update TitleUIManager.cs
    フィールド変数の名称を変更した。

--- ThinkAutonomouslyTurret/Assets/Scripts/TitleScene/TitleUIManager.cs
+++ ThinkAutonomouslyTurret/Assets/Scripts/TitleScene/TitleUIManager.cs
@@ -9,16 +9,20 @@ public class TitleUIManager : SingletonMonoBehaviour<TitleUIManager>
     #region Refarences
     // タイトルのテキストコンポーネント
     [SerializeField]
-    private TextMeshProUGUI _titleTextComponent = null;
-    // タイトルに表示する文字
-    [SerializeField]
-    private string _titleText = "自律思考型固定砲台";
+    private TextMeshProUGUI _titleText = null;
     // ゲームのスタートを案内するテキストコンポーネント
     [SerializeField]
-    private TextMeshProUGUI _gameStartTextComponent = null;
+    private TextMeshProUGUI _gameStartText = null;
+    #endregion
+    #region Message
+    // タイトルに表示する文字
+    [SerializeField]
+    private string _titleMessage = "自律思考型固定砲台";
     // ゲームのスタートを案内するテキストに表示する文字
     [SerializeField]
-    private string _gameStartText = "Spece を押したらスタート";
+    private string _gameStartMessage = "Spece を押したらスタート";
+    #endregion
+    #region DurationTimeSettings
     // 降下アニメーションの再生時間
     [SerializeField, Range(0.0f, 270.0f)]
     private float _movedownAnimationTime = 90.0f;
@@ -26,15 +30,17 @@ public class TitleUIManager : SingletonMonoBehaviour<TitleUIManager>
     [SerializeField]
     private float _blinkAnimationTime = 5.0f;
     #endregion
-
+    #region TextSettings
     // タイトルテキストのフォントサイズ
     private readonly float _titleFontSize = 180.0f;
     // ゲームスタートの案内テキストのフォントサイズ
     private readonly float _gameStartFontSize = 60.0f;
-
+    #endregion
+    #region CancellationToken
     // キャンセル処理用のトークン
     CancellationTokenSource _titleAnimationToken = new CancellationTokenSource();
     CancellationTokenSource _gameStartAnimationToken = new CancellationTokenSource();
+    #endregion
 
     // Start is called before the first frame update
     private async void Start()
@@ -42,15 +48,15 @@ public class TitleUIManager : SingletonMonoBehaviour<TitleUIManager>
         // null チェック
         var canvas = GameObject.Find("TitleSceneCanvas").GetComponent<Canvas>();
         var uiCamera = canvas.worldCamera;
-        if(canvas.worldCamera == null || uiCamera == null)
+        if (canvas.worldCamera == null || uiCamera == null)
         {
-            uiCamera= GameObject.Find("UICamera").GetComponent<Camera>();
+            uiCamera = GameObject.Find("UICamera").GetComponent<Camera>();
             canvas.worldCamera = uiCamera;
         }
         SetUpText();
         await MoveDownTextAnimation(_movedownAnimationTime, _titleAnimationToken);
         TitleSceneManager.Instance.IsActivatedChangeScene();
-        _gameStartTextComponent.gameObject.SetActive(true);
+        _gameStartText.gameObject.SetActive(true);
         await BlinkTextAnimation(_blinkAnimationTime, _gameStartAnimationToken);
     }
 
@@ -60,22 +66,22 @@ public class TitleUIManager : SingletonMonoBehaviour<TitleUIManager>
     private void SetUpText()
     {
         // タイトルテキストの null チェック
-        if (_titleTextComponent == null)
+        if (_titleText == null)
         {
-            _titleTextComponent = GameObject.Find("GameTitle").GetComponent<TextMeshProUGUI>();
+            _titleText = GameObject.Find("GameTitle").GetComponent<TextMeshProUGUI>();
         }
-        _titleTextComponent.fontSize = _titleFontSize;
-        _titleTextComponent.rectTransform.anchoredPosition = Vector2.up * _titleTextComponent.rectTransform.rect.height;
-        _titleTextComponent.text = _titleText;
+        _titleText.fontSize = _titleFontSize;
+        _titleText.rectTransform.anchoredPosition = Vector2.up * _titleText.rectTransform.rect.height;
+        _titleText.text = _titleMessage;
         // ゲームスタートテキストの null チェック
-        if (_gameStartTextComponent == null)
+        if (_gameStartText == null)
         {
-            _gameStartTextComponent = GameObject.Find("GameStart").GetComponent<TextMeshProUGUI>();
+            _gameStartText = GameObject.Find("GameStartText").GetComponent<TextMeshProUGUI>();
         }
-        _gameStartTextComponent.fontSize = _gameStartFontSize;
-        _gameStartTextComponent.alignment = TextAlignmentOptions.Center;
-        _gameStartTextComponent.text = _gameStartText;
-        _gameStartTextComponent.gameObject.SetActive(false);
+        _gameStartText.fontSize = _gameStartFontSize;
+        _gameStartText.alignment = TextAlignmentOptions.Center;
+        _gameStartText.text = _gameStartMessage;
+        _gameStartText.gameObject.SetActive(false);
     }
 
     /// <summary>
@@ -87,7 +93,7 @@ public class TitleUIManager : SingletonMonoBehaviour<TitleUIManager>
     private async UniTask MoveDownTextAnimation(float moveDownAnimationTime, CancellationTokenSource cts = default)
     {
         // テキストの最終的な Y 座標
-        RectTransform rectTransform = _titleTextComponent.rectTransform;
+        RectTransform rectTransform = _titleText.rectTransform;
         float titleTextYPosition = -90.0f;
         while (rectTransform.anchoredPosition.y > titleTextYPosition)
         {
@@ -115,7 +121,7 @@ public class TitleUIManager : SingletonMonoBehaviour<TitleUIManager>
     /// <returns></returns>
     private async UniTask BlinkTextAnimation(float blinkAnimationDuration, CancellationTokenSource cts = default)
     {
-        Color textColor = _gameStartTextComponent.color;
+        Color textColor = _gameStartText.color;
         float alpha = 0.0f;
         // テキストが表示されているかの変数
         bool isPreviewedText = false;
@@ -129,7 +135,7 @@ public class TitleUIManager : SingletonMonoBehaviour<TitleUIManager>
                 {
                     case true:
                         alpha -= Time.deltaTime / blinkAnimationDuration * 2;
-                        _gameStartTextComponent.color = new Color(textColor.r, textColor.g, textColor.b, alpha);
+                        _gameStartText.color = new Color(textColor.r, textColor.g, textColor.b, alpha);
                         if (alpha <= 0.0f)
                         {
                             alpha = 0.0f;
@@ -138,7 +144,7 @@ public class TitleUIManager : SingletonMonoBehaviour<TitleUIManager>
                         break;
                     case false:
                         alpha += Time.deltaTime / blinkAnimationDuration * 2;
-                        _gameStartTextComponent.color = new Color(textColor.r, textColor.g, textColor.b, alpha);
+                        _gameStartText.color = new Color(textColor.r, textColor.g, textColor.b, alpha);
                         if (alpha >= 1.0f)
                         {
                             alpha = 1.0f;

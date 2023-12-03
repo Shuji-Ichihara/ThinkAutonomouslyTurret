f5919209fb62b6ce6846be186418c8772a0e993e
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-12-04 00:55:40 +0900

    Update ResultUIManager.cs
    テキストが点滅するアニメーションを追加した。

--- ThinkAutonomouslyTurret/Assets/Scripts/ResultScene/ResultUIManager.cs
+++ ThinkAutonomouslyTurret/Assets/Scripts/ResultScene/ResultUIManager.cs
@@ -1,6 +1,5 @@
 ﻿using Cysharp.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using System.Threading;
 using TMPro;
 using UnityEngine;
@@ -32,6 +31,9 @@ public class ResultUIManager : SingletonMonoBehaviour<ResultUIManager>
     // タイトルの案内テキストを表示するまで待機する時間
     [SerializeField]
     private float _secondDurationTime = 2.0f;
+    // テキストの点滅間隔
+    [SerializeField]
+    private float _blinkDurationTime = 1.0f;
     #endregion
     #region TextSettings
     [SerializeField]
@@ -89,6 +91,7 @@ public class ResultUIManager : SingletonMonoBehaviour<ResultUIManager>
         await UniTask.WaitForSeconds(_secondDurationTime, cancellationToken: cts.Token);
         AudioManager.Instance.PlayBGM(BGMType.ResultBGM);
         _returnToStartText.gameObject.SetActive(true);
+        CallBlinkTextAnimation();
         ResutlSceneManager.Instance.IsActivatedChageScene();
     }
 
@@ -99,5 +102,64 @@ public class ResultUIManager : SingletonMonoBehaviour<ResultUIManager>
     {
         CancellationTokenSource previewTextToken = new CancellationTokenSource();
         DisplayTextInOrder(previewTextToken).Forget();
+    }
+
+    /// <summary>
+    /// BlickTextAnimation を呼び出す関数
+    /// </summary>
+    private void CallBlinkTextAnimation()
+    {
+        CancellationTokenSource blinkTextAnimationToken = new CancellationTokenSource();
+        BlinkTextAnimation(_blinkDurationTime, blinkTextAnimationToken).Forget();
+
+    }
+
+    /// <summary>
+    /// テキストの点滅アニメーション
+    /// </summary>
+    /// <param name="blinkAnimationDuration">点滅にかかる時間</param>
+    /// <param name="cts">キャンセル処理用のトークン</param>
+    /// <returns></returns>
+    private async UniTask BlinkTextAnimation(float blinkAnimationDuration, CancellationTokenSource cts = default)
+    {
+        Color textColor = _returnToStartText.color;
+        float alpha = 0.0f;
+        // テキストが表示されているかの変数
+        bool isPreviewedText = false;
+        // タイトルシーンがアクティブである限りループする
+        while (true)
+        {
+            try
+            {
+                // true の場合は透明、false の場合は不透明に近づける
+                switch (isPreviewedText)
+                {
+                    case true:
+                        alpha -= Time.deltaTime / blinkAnimationDuration * 2;
+                        _returnToStartText.color = new Color(textColor.r, textColor.g, textColor.b, alpha);
+                        if (alpha <= 0.0f)
+                        {
+                            alpha = 0.0f;
+                            isPreviewedText = false;
+                        }
+                        break;
+                    case false:
+                        alpha += Time.deltaTime / blinkAnimationDuration * 2;
+                        _returnToStartText.color = new Color(textColor.r, textColor.g, textColor.b, alpha);
+                        if (alpha >= 1.0f)
+                        {
+                            alpha = 1.0f;
+                            isPreviewedText = true;
+                        }
+                        break;
+                }
+                await UniTask.Yield(cts.Token);
+            }
+            catch (Exception)
+            {
+                continue;
+            }
+        }
+
     }
 }
